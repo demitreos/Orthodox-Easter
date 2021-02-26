7a498d5a69ce657076cb05ca38613b27547c1b6d
demitreos <79422264+demitreos@users.noreply.github.com>
2021-02-27 00:10:10 +0200

    Update getOrthodoxEaster.js

--- getOrthodoxEaster.js
+++ getOrthodoxEaster.js
@@ -1,6 +1,6 @@
 function getEaster(year)
 {
-	var dTmp, iTmp, day;
+	var dTmp, day, dTmp2, mm, dd;
 	
 	var x = year - 2;
 	var y = x % 19;
@@ -8,39 +8,46 @@ function getEaster(year)
 	var E = z % 30;
 	var k = 44 - E;
 	if(E > 23)
-		dTmp = new Date(year, 3, k);
+		dTmp = new Date(year, 3, k + 13);
 	else
-		dTmp = new Date(year, 2, k);
-	iTmp = dTmp.getTime() + (13 * 24 * 60 * 60 * 1000);
+		dTmp = new Date(year, 2, k + 13);
+		
+	dd = dTmp.getDate();
+	mm = dTmp.getMonth();
+	dTmp2 = new Date(year, mm, dd);
 	
-	dTmp = new Date(iTmp);
+	
 	day = dTmp.getDay();
 	
 	switch(day){
 		case 0: //SUNDAY
-			iTmp = iTmp + (7 * 24 * 60 * 60 * 1000); 
+			if(E != 24 && E!= 25)
+				dTmp2 = new Date(year, mm, dd + 7);
 			break;
 		case 1: //MONDAY
-			iTmp = iTmp + (6 * 24 * 60 * 60 * 1000);
+			if(E != 24)
+				dTmp2 = new Date(year, mm, dd + 6);
+			else
+				dTmp2 = new Date(year, mm, dd - 1);
 			break;
 		case 2: //TUESDAY
-			iTmp = iTmp + (5 * 24 * 60 * 60 * 1000);
+			dTmp2 = new Date(year, mm, dd + 5);
 			break;
 		case 3: //WEDNESDAY
-			iTmp = iTmp + (4 * 24 * 60 * 60 * 1000);
+			dTmp2 = new Date(year, mm, dd + 4);
 			break;
 		case 4: //THURSDAY
-			iTmp = iTmp + (3 * 24 * 60 * 60 * 1000);
+			dTmp2 = new Date(year, mm, dd + 3);
 			break;
 		case 5: //FRIDAY
-			iTmp = iTmp + (2 * 24 * 60 * 60 * 1000);
+			dTmp2 = new Date(year, mm, dd + 2);
 			break;
 		case 6: //SATURDAY
-			iTmp = iTmp + (1 * 24 * 60 * 60 * 1000);
+			dTmp2 = new Date(year, mm, dd + 1);
 			break;
 		default:
 			break;
 	}
 	
-	return new Date(iTmp);
+	return dTmp2;
 }
